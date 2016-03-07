ecf7f26c43053c8c81bc1565618c7aad95dcde38
Артур Асылгареев <virid.raven@gmail.com>
2016-03-07 18:05:29 +0500

    Предварительная инициализация репозитория

---
+++ make/Makefile.mak
@@ -0,0 +1,56 @@
+#Makefile
+SRCDIR := source
+
+OBJDIR := obj
+
+BINDIR := bin
+
+HEXDIR := hex
+
+CC := avr-gcc
+FLAGS := -Os -DFF_CPU=16000000UL -mmcu=atmega328p -ffreestanding -c
+
+HEXDIR_REL := $(addprefix ../, $(HEXDIR))
+
+BINDIR_REL := $(addprefix ../, $(BINDIR))
+
+SRCDIR_REL := $(addprefix ../, $(SRCDIR))
+
+SRCFILES := $(wildcard $(addsuffix /*.c, $(SRCDIR_REL)))
+
+OBJDIR_REL := $(addprefix ../, $(OBJDIR))
+
+OBJFILES := $(patsubst $(SRCDIR_REL)/%.c,$(OBJDIR_REL)/%.o,$(wildcard $(SRCFILES)))
+
+default:
+	@echo -e "\e[0;33mmake mStation\e[0;32m - компиляция станции"
+	@echo -e "\e[0;33mmake clean\e[0;32m - чистка объектных файлов"
+	@echo -e "\e[0;33mmake delete\e[0;32m - удаление исполняемых файлов" 
+	
+mStation:$(BINDIR_REL)/mStation $(HEXDIR_REL)/mStation.hex  $(OBJFILES)  
+	@echo -e "Компиляция mpi программы успешна\n"
+
+$(HEXDIR_REL)/mStation.hex: $(BINDIR_REL)/mStation
+	avr-objcopy -O ihex -R .eeprom $(BINDIR_REL)/mStation $(HEXDIR_REL)/mStation.hex
+
+$(BINDIR_REL)/mStation: $(OBJFILES) 
+	$(CC) -mmcu=atmega328p $^ -o $@ -lm
+
+	
+
+$(OBJDIR_REL)/%.o: $(SRCDIR_REL)/%.c
+	$(CC) $<  $(FLAGS) $(addprefix -I, $(SRCDIR_REL)) -o $@ -pipe -lm
+	
+include  $(wildcard $(OBJDIR_REL)/*.d)
+#Все пользователи имеют право читать копируется в каталог
+clean:
+	@rm -f $(OBJDIR_REL)/*.o $(OBJDIR_REL)/*.d
+	@rm -f $(OBJDIR_REL_S)/*.o $(OBJDIR_REL_S)/*.d
+	@rm -f $(OBJDIR_REL_D)/*.o $(OBJDIR_REL_D)/*.d
+	@rm -f $(BINDIR_REL)/*
+	@echo "Очистка завершена!"
+delete:
+	@echo "Клиент удалён!"
+	@rm -f $(BINDIR_REL)
+	@echo "Сервер удалён!"
+.PHONY: clean delete mStation
---
+++ source/main.c
@@ -0,0 +1,70 @@
+#define F_CPU 16000000UL
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#define USART_BAUDRATE 9600
+#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+
+void USART0Init(void)
+{
+    // Set baud rate
+    UBRR0H = (uint8_t)(UBRR_VALUE>>8);
+    UBRR0L = (uint8_t)UBRR_VALUE;
+    // Set frame format to 8 data bits, no parity, 1 stop bit
+    UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00);
+    //enable transmission and reception
+    UCSR0B |= (1<<RXEN0)|(1<<TXEN0);
+    }
+    int USART0SendByte(char u8Data, FILE *stream)
+    {
+       if(u8Data == '\n')
+       {
+            USART0SendByte('\r', stream);
+       }
+    //wait while previous byte is completed
+    while(!(UCSR0A&(1<<UDRE0))){};
+    // Transmit data
+    UDR0 = u8Data;
+    return 0;
+}
+//set stream pointer
+FILE usart0_str = FDEV_SETUP_STREAM(USART0SendByte, NULL, _FDEV_SETUP_WRITE);
+
+void InitADC()
+{
+ // Select Vref=AVcc
+ ADMUX |= (1<<REFS0);
+ //set prescaller to 128 and enable ADC 
+ ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);    
+}
+
+uint16_t ReadADC(uint8_t ADCchannel)
+{
+ //select ADC channel with safety mask
+ ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+ //single conversion mode
+ ADCSRA |= (1<<ADSC);
+ // wait until ADC conversion is complete
+ while( ADCSRA & (1<<ADSC) );
+ return ADC;
+}
+
+
+int main(void)
+{
+    double value = 0.0;
+    //initialize ADC
+    InitADC();
+    //Initialize USART0
+    USART0Init();
+    //assign our stream to standart I/O streams
+    stdout=&usart0_str;
+    
+    while(1)
+    {
+        value =(double)0.5*ReadADC(PORTC0); 
+        
+        printf("%u\n", (uint16_t)value);
+    }
+}

1299620de1b19ba74be80aa8168f4cd66f146b75
Артур Асылгареев <virid.raven@gmail.com>
2016-03-09 22:59:32 +0500

    Добавлен файл с кодами ошибок

---
+++ source/stationError.h
@@ -0,0 +1,9 @@
+#ifndef STATION_ERROR_H
+#define STATION_ERROR_H
+#define STATUS_OK 0
+#define UNKNW_ERROR 1
+#define INIT_ERROR 2
+#define READ_ERROR 3
+#define CRITICAL_ERROR 4
+extern unsigned char stationError;
+#endif

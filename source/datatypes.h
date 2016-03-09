7197bf68492733e3fd3d70554ff74c1e89b4cb84
Артур Асылгареев <virid.raven@gmail.com>
2016-03-09 22:58:59 +0500

    Добавлен отдельный тип данных для LM35. Убраны лишние поля в SensorsData

--- source/datatypes.h
+++ source/datatypes.h
@@ -1,14 +1,16 @@
 #ifndef DATATYPES_H
 #define DATATYPES_h
 typedef struct SensorsData sensorsData;
+typedef struct Lm35Data lm35Data;
+
+struct Lm35Data
+{
+    char Temp;
+    char errorCode;
+};
 
 struct SensorsData
 {
-    int lm35Temp;
-    double bmp280Pressure;
-    double bmp280Temp;
-    double bmp280Altitude;
-    double dht22Temp;
-    double dht22Humidity;
+    lm35Data lm35;
 };
 #endif

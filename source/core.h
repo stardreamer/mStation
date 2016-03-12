3720921002ec7fbf5278db9430d084cffe115614
Артур Асылгареев <virid.raven@gmail.com>
2016-03-12 12:21:26 +0500

    Изменен порядок define для улучшения читаемости кода

--- source/core.h
+++ source/core.h
@@ -1,8 +1,8 @@
 #ifndef CORE_H
 #define CORE_H
 #define F_CPU 16000000UL
-#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 #define USART_BAUDRATE 9600
+#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>

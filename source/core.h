ab217be8f0a031ac4ca2fd87887729523dfef4ae
Артур Асылгареев <virid.raven@gmail.com>
2016-04-05 22:08:34 +0500

    Удален старый код перед началом реализации нового протокола на основе прерываний

--- source/core.h
+++ source/core.h
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <util/delay.h>
 #include "datatypes.h"
-#include "dht22.h"
 
 void USART0Init(void);
 int USART0SendByte(char u8Data, FILE *stream);
--- source/main.c
+++ source/main.c
@@ -11,19 +11,4 @@ int main(void)
     USART0Init();
     //assign our stream to standart I/O streams
     stdout=&usart0_str;
-    
-    dht22Data mydata = {
-        .integralTemp = 0,
-        .fracTemp = 0,
-        .integralHumidity = 0,
-        .fracHumidity = 0,
-        .pinNumber = 1,
-        .errorCode = DHT_NOT_INIT};
-        
-    dht_init(&mydata);
-    
-    while(1)
-    {
-        printf("%i %i %i\n", mydata.integralTemp, mydata.pinNumber, mydata.errorCode);
-    }
 }
